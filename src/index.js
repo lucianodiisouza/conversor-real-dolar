c78ec41f62c218abb6691e2af08d3a83684bacd0
Luciano dii Souza <contato@thecodelovers.com.br>
2020-03-07 22:08:06 -0300

    At this example i built a simple currency converter between U13357 DOLAR and BRL R$, using state share between elements inside a component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './Index.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const COTACAO = 4.6; //Dolar Quotation at 07.03.2020 - 21:26hrs
+const convertaDolarEmReal = dolar =>
+    dolar * COTACAO;
+const convertaRealEmDolar = real =>
+    real / COTACAO;
+
+const InputDeValor = ({
+    moeda,
+    valor,
+    onChange
+}) => {
+    return (
+        <div>
+            <input
+                type="number"
+                value={valor || ''}
+                onChange={e => onChange(e.target.value)}
+                className="input"
+                placeholder={moeda}
+            />
+        </div>
+    );
+};
+
+const App = props => {
+    const [
+        valorEmDolar,
+        setValorEmDolar
+    ] = React.useState();
+
+    return (
+        <form className="main">
+            <InputDeValor
+                moeda="R$"
+                valor={convertaDolarEmReal(valorEmDolar)}
+                onChange={v =>
+                    setValorEmDolar(convertaRealEmDolar(v))
+                }
+            />
+            <InputDeValor
+                moeda="US$"
+                valor={valorEmDolar}
+                onChange={v => setValorEmDolar(v)}
+            />
+        </form>
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
